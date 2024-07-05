650eefc6adfe9aebfc65e193324a6444bb9a0c77
SatyrDiamond <109001272+SatyrDiamond@users.noreply.github.com>
2024-07-05 14:57:56 -0400

    Add files via upload

---
+++ src/brr_codec.h
@@ -0,0 +1,13 @@
+/*
+ *  brrcodec.h
+ *  C700
+ *
+ *  Created by osoumen on 06/11/06.
+ *  Copyright 2006 Vermicelli Magic. All rights reserved.
+ *
+ */
+
+int brrencode(short *input_data, unsigned char *output_data, long inputframes, bool isLoop, long loop_point, int pad_frames);
+int brrdecode(unsigned char *src, short *output, int looppoint, int loops);
+int checkbrrsize(unsigned char *src, int *size);
+int emphasis(short *data, unsigned int length);
