58a275c26721b6309fb50e5e6659d0a7228efc5c
SatyrDiamond <109001272+SatyrDiamond@users.noreply.github.com>
2024-07-10 23:14:57 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@ gcc src/oki_codec.c -shared -o libs/oki_codec.dll
 gcc src/yma_codec.c -shared -o libs/yma_codec.dll
 gcc src/ymb_codec.c -shared -o libs/ymb_codec.dll
 gcc src/ymz_codec.c -shared -o libs/ymz_codec.dll
-g++ src/brr_codec.cpp -shared -o libs/brr_codec.dll
+g++ src/brr_codec.cpp -shared -o libs/brr_codec.dll -static
